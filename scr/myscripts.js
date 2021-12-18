b123b23c7e0c00b3c3a2bd7ae43efe1b7d0a45cd
Anton Barrera Mora <84127544+Kamaranis@users.noreply.github.com>
2021-12-18 16:22:14 +0900

    Traslado de contenidos
    Probando si esto funciona

---
+++ scr/myscripts.js
@@ -0,0 +1,13 @@
+/*
+function big (x) {
+    x.style.height = "462px";
+    x.style.width = "210px";
+}
+
+function small (x) {
+    x.style.height = "412px";
+    x.style.width =  "159px";
+}
+*/
+
+//all var from the DOM
